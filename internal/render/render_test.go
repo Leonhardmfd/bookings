db247510e6e9db221c3fe07367d6c03d3a99d533
Leonhardmfd <leonhard.mfd@gmail.com>
2023-01-25 11:40:12 +0100

    update all + Add js

--- internal/render/render_test.go
+++ internal/render/render_test.go
@@ -11,12 +11,13 @@ func TestAddDefaultData(t *testing.T) {
 
 	r, err := getSession()
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
 	session.Put(r.Context(), "flash", "123")
 
 	result := AddDefaultData(&td, r)
+
 	if result.Flash != "123" {
 		t.Error("flash value of 123 not found in session")
 	}
@@ -40,7 +41,7 @@ func TestRenderTemplate(t *testing.T) {
 
 	err = Template(&ww, r, "home.page.gohtml", &models.TemplateData{})
 	if err != nil {
-		t.Error("error writing template to browser", err)
+		t.Error("error writing template to browser")
 	}
 
 	err = Template(&ww, r, "non-existent.page.gohtml", &models.TemplateData{})
@@ -58,6 +59,7 @@ func getSession() (*http.Request, error) {
 	ctx := r.Context()
 	ctx, _ = session.Load(ctx, r.Header.Get("X-Session"))
 	r = r.WithContext(ctx)
+
 	return r, nil
 }
 
@@ -67,7 +69,6 @@ func TestNewTemplates(t *testing.T) {
 
 func TestCreateTemplateCache(t *testing.T) {
 	pathToTemplates = "./../../templates"
-
 	_, err := CreateTemplateCache()
 	if err != nil {
 		t.Error(err)
